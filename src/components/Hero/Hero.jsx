2a3c8f6d304644243a13c6046af618fa2a80bea4
aneta-s <54153719+aneta-s@users.noreply.github.com>
2021-03-18 20:28:02 +0100

    uploading seo

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -28,7 +28,7 @@ const Header = () => {
             {title || 'Hi, my name is'}{' '}
             <span className="text-color-main">{name || 'Aneta Stojanowska, '}</span>
             <br />
-            {subtitle || "I'm a Product Designer."}
+            {subtitle || 'based in Amsterdam.'}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
